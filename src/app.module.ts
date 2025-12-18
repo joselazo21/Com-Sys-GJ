161662244da692627d1babecb460127f93395a29
joselazo21 <josemoraleslazo21@gmail.com>
2025-12-17 20:01:24 -0500

    fix: db config sqlite

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,15 +11,25 @@ import { SalesModule } from './sales/sales.module';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    TypeOrmModule.forRoot({
-      type: process.env.DB_TYPE as any,  // 'postgres', 'mysql', 'sqlite', etc.
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : undefined,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: process.env.NODE_ENV !== 'production',
+    TypeOrmModule.forRootAsync({
+      useFactory: () => {
+        const commonConfig = {
+          type: process.env.DB_TYPE as any,
+          database: process.env.DB_DATABASE,
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          synchronize: process.env.NODE_ENV !== 'production',
+        };
+        if (process.env.DB_TYPE !== 'sqlite') {
+          return {
+            ...commonConfig,
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : undefined,
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+          };
+        }
+        return commonConfig;
+      },
     }),
     AuthModule,
     UsersModule,
